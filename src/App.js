655a144a0b1919e4ed4b7413d8ce53933e7210d2
handy-o <dyhan9420@gmail.com>
2022-07-31 11:15:11 +0900

    컴포넌트 생성 및 scss연결

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import '../scss/style.scss';
+import Btns from './Btns';
+import Panels from './Panels';
+
+function App() {
+	return (
+		<main>
+			<h1>TYPOGRAPHY</h1>
+			<Btns />
+			<Panles />
+		</main>
+	);
+}
+
+export default App;
---
+++ src/Panels.js
@@ -0,0 +1,23 @@
+function Panels() {
+	return (
+		<ul>
+			<li>
+				<div className='bg'></div>
+			</li>
+			<li>
+				<div className='bg'></div>
+			</li>
+			<li>
+				<div className='bg'></div>
+			</li>
+			<li>
+				<div className='bg'></div>
+			</li>
+			<li>
+				<div className='bg'></div>
+			</li>
+		</ul>
+	);
+}
+
+export default Panels;
